94432001f35c071e4139ff2c319dceb2bb081739
leonardo_Wer <name89213126414@gmail.com>
2025-07-11 19:58:09 +0300

    Анимации появления

--- js/index.js
+++ js/index.js
@@ -6,11 +6,13 @@ document.addEventListener('DOMContentLoaded', function () {
 function initHeader() {
     const header = document.querySelector('.header__scroll-bg');
     const headerHeight = header.offsetHeight;
+    const topMenu = document.querySelector('.top-menu');
 
     function checkScroll() {
         let scrollPosition = window.scrollY;
         let opacity = 0;
 
+        // Затемнение хедера
         if (scrollPosition < headerHeight) {
             opacity = scrollPosition / headerHeight * 0.8;
         } else {
@@ -19,6 +21,15 @@ function initHeader() {
 
         // Устанавливаем прозрачность scroll-bg
         header.style.opacity = opacity;
+
+        // Верхнее меню
+        if (scrollPosition >= headerHeight * 0.4) {
+            topMenu.classList.add('active');
+            topMenu.classList.remove('hidden');
+        } else {
+            topMenu.classList.add('hidden');
+            topMenu.classList.remove('active');
+        }
     }
 
     // Привязываем функцию checkScroll к событию прокрутки
