a01dbfcfe0b2a7e68ea195dc87df413434925cc0
leonardo_Wer <name89213126414@gmail.com>
2025-07-15 14:26:50 +0300

    Анимация картинки

--- js/index.js
+++ js/index.js
@@ -1,9 +1,3 @@
-document.addEventListener('DOMContentLoaded', function () {
-    initHeader();
-    initAnimations()
-});
-
-
 // Затемнение хедера
 function initHeader() {
     const header = document.querySelector('.header__scroll-bg');
@@ -66,6 +60,24 @@ function handleIntersection(entry, observer) {
             const target = +container.dataset.target;
             const duration = +container.dataset.duration || 2000;
 
+            function animateCounter(counter, target, duration) {
+                const increment = target / (duration / 16);
+                let current = 0;
+
+                function updateCounter() {
+                    current += increment;
+                    counter.textContent = Math.floor(current);
+
+                    if (current < target) {
+                        requestAnimationFrame(updateCounter);
+                    } else {
+                        counter.textContent = target;
+                    }
+                }
+
+                updateCounter();
+            }
+
             animateCounter(counter, target, duration);
         }
 
@@ -73,20 +85,69 @@ function handleIntersection(entry, observer) {
     }
 }
 
-function animateCounter(counter, target, duration) {
-    const increment = target / (duration / 16);
-    let current = 0;
 
-    function updateCounter() {
-        current += increment;
-        counter.textContent = Math.floor(current);
+// Движение картинки за мышкой
+function initRotationElementMouseMoving() {
+    const element = document.querySelector('.bcd-container-center__left-bc-img');
 
-        if (current < target) {
-            requestAnimationFrame(updateCounter);
-        } else {
-            counter.textContent = target;
-        }
+    // Параметры анимации
+    const ease = 0.1; // Скорость движения
+    const rotationSensitivity = 7 // Чувствительность поворота
+
+    // Текущий поворот
+    let currentX = 0
+    let currentY = 0
+
+    // Результат поворота
+    let targetX = 0;
+    let targetY= 0;
+
+    function animate() {
+        // Меняем координаты
+        currentX += (targetX - currentX) * ease;
+        currentY += (targetY - currentY) * ease;
+
+        // Применяем трансформацию
+        element.style.transform = `rotateX(${currentX}deg) rotateY(${currentY}deg)`;
+
+        // Повторяем каждый кадр
+        requestAnimationFrame(animate);
     }
 
-    updateCounter();
+    // Мышка
+    element.addEventListener('mousemove', (e) => {
+        // Позиция мышки внутри карточки
+        const rect = element.getBoundingClientRect();
+
+        // Координаты мыши относительно верхнего левого угла карточки
+        const x = e.clientX - rect.left;
+        const y = e.clientY - rect.top;
+
+        // Центр карточки
+        const centerX = rect.width / 2;
+        const centerY = rect.height / 2;
+
+        // Наклон от центра
+        targetX = (centerY - y) / rotationSensitivity;
+        targetY = (x - centerX) / rotationSensitivity;
+
+        animate();
+    });
+
+    element.addEventListener('mouseleave', () => {
+        targetX = 0;
+        targetY = 0;
+    });
+
+    // Запускаем анимацию
+    animate();
 }
+
+
+// Загрузка страницы
+document.addEventListener('DOMContentLoaded', function () {
+    initHeader();
+    initAnimations();
+});
+
+initRotationElementMouseMoving();
